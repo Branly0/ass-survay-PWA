fa3146e6badc366d1d36597e95d41a5f6c8a90ff
Branly0 <nkasibranly01@gmail.com>
2026-04-12 05:53:39 +0100

    .

---
+++ BACK/app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../.env"))
+
+class Settings(BaseSettings):
+    DB_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    CORS_ORIGIN = ["*"]
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+    
+settings = Settings()
