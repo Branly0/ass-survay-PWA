2eff1e085f56e7620373c8b8bc4c20b07737eb10
Branly0 <nkasibranly01@gmail.com>
2026-04-14 15:52:32 -0300

    websocket from backend

--- BACK/app/apis/filler.py
+++ BACK/app/apis/filler.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, WebSocket, WebSocketDisconnect
+from pydantic import json
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.model import survey
@@ -6,12 +7,24 @@ from app.schema import survey as survey_schema
 
 router = APIRouter(prefix="/filler", tags=["filler"])
 
+connected_owner: list[WebSocket] = []
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connected_owner.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        connected_owner.remove(websocket)
+
 @router.get("/")
 def get_fillers():
     return {"message": "Get filler data"}
 
 @router.post("/submit", response_model=survey_schema.SurveyResponse)
-def submit_survey(survey_data: survey_schema.Surveyfiller, db: Session = Depends(get_db)):
+async def submit_survey(survey_data: survey_schema.Surveyfiller, db: Session = Depends(get_db)):
     if survey_data.age_group not in ["child", "teen", "adult", "senior"]:
         raise HTTPException(status_code=400, detail="Invalid age group try [child, teen, adult, senior]")
     if survey_data.gender not in ["male", "female"]:
@@ -22,6 +35,8 @@ def submit_survey(survey_data: survey_schema.Surveyfiller, db: Session = Depends
         gender=survey_data.gender,
         nationality=survey_data.nationality
     )
+    for client in connected_owner:
+        await client.send_text(json.dumps({"event": "new_survey", "data": survey_data.dict()}))
     db.add(new_survey)
     db.commit()
     db.refresh(new_survey)
